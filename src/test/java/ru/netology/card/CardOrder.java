147c334a966183f653c3a1ad99b471717632505f
KuliakQA <culeacvadim@gmail.com>
2022-01-03 21:30:56 +0100

    add tests

--- src/test/java/ru/netology/card/CardOrder.java
+++ src/test/java/ru/netology/card/CardOrder.java
@@ -8,16 +8,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardOrder {
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
     @BeforeAll
     public static void setUpAll(){
         System.setProperty("webdriver.chrome.driver","./driver/win/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @BeforeEach
