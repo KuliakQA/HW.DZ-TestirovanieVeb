31135e2f29e5affe7745c01d60e50c1d6ad079a5
KuliakQA <culeacvadim@gmail.com>
2022-01-04 14:28:41 +0100

    add tests

--- src/test/java/ru/netology/card/CardOrder.java
+++ src/test/java/ru/netology/card/CardOrder.java
@@ -17,18 +17,17 @@ public class CardOrder {
     private static WebDriver driver;
 
     @BeforeAll
-    public static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver","./driver/win/chromedriver.exe");
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-    }
-
-    @BeforeEach
-    public void setUp() {
-        driver = new ChromeDriver();
     }
 
     @AfterEach
